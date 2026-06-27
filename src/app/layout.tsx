5f27131907d6137ec6db1b654323d4bf11b6ab1f
Ryan Ferreira <heyryanf@hotmail.com>
2026-06-27 13:52:25 -0300

    style: Match exact background color from reference

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -28,7 +28,7 @@ export default function RootLayout({
       lang="en"
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
     >
-      <body className="min-h-full flex flex-col bg-[#FCFBF8]">
+      <body className="min-h-full flex flex-col bg-[#fcfaf7]">
         <NavMenu />
         <main className="flex-1 flex flex-col">
           {children}
