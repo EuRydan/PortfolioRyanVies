0f187b569bd8d545ef79d975df52b5a2565aeba7
Ryan Ferreira <heyryanf@hotmail.com>
2026-06-27 14:11:02 -0300

    style: Remove 'We do' and increase size of rolling text

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,8 @@
 export default function Home() {
   return (
     <div className="flex flex-col items-center justify-center flex-1 font-sans">
-      <div className="text-4xl md:text-7xl font-bold flex flex-col items-center gap-4 text-center">
-        <span>We do</span>
-        <div className="h-[1.2em] overflow-hidden text-black/70">
+      <div className="text-6xl md:text-9xl font-bold flex flex-col items-center justify-center text-center tracking-tight">
+        <div className="h-[1.2em] overflow-hidden text-black/90">
           <ul className="animate-roll flex flex-col leading-[1.2em]">
             <li>/Design</li>
             <li>/Branding</li>
